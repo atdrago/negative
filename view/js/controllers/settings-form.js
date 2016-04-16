a00fbbb08fab3dcba336b2505ac87457206ddad9
Adam Drago <atdrago@gmail.com>
2016-04-16 01:17:20 -0700

    Convert spaces to tabs

--- view/js/controllers/settings-form.js
+++ view/js/controllers/settings-form.js
@@ -1,16 +1,16 @@
 const { ipcRenderer } = require('electron');
 
 class SettingsForm {
-    constructor() {
-        this.shouldShowTips = document.getElementById('shouldShowTips');
+	constructor() {
+		this.shouldShowTips = document.getElementById('shouldShowTips');
 
-        this.shouldShowTips.addEventListener('change', function (evt) {
-            ipcRenderer.send('set-settings-request', { shouldShowTips: evt.target.checked });
-        });
+		this.shouldShowTips.addEventListener('change', function (evt) {
+			ipcRenderer.send('set-settings-request', { shouldShowTips: evt.target.checked });
+		});
 
-        ipcRenderer.send('get-settings-request');
-        ipcRenderer.on('get-settings-response', (evt, settings) => {
-            this.shouldShowTips.checked = (settings['shouldShowTips'] !== false);
-        });
-    }
+		ipcRenderer.send('get-settings-request');
+		ipcRenderer.on('get-settings-response', (evt, settings) => {
+			this.shouldShowTips.checked = (settings['shouldShowTips'] !== false);
+		});
+	}
 }
--- view/js/services/negative-undo.js
+++ view/js/services/negative-undo.js
@@ -1,45 +1,45 @@
 const SimpleUndo = require('simple-undo');
 
 class UndoManager {
-    constructor() {
-        this.state = {
-            imageDimensions: null,
-            imageSrc: null
-        };
+	constructor() {
+		this.state = {
+			imageDimensions: null,
+			imageSrc: null
+		};
 
-        this.history = new SimpleUndo({
-            maxLength: 10,
-            provider: (done) => done(JSON.stringify(this.state))
-        });
-        this.history.initialize(JSON.stringify(this.state));
+		this.history = new SimpleUndo({
+			maxLength: 10,
+			provider: (done) => done(JSON.stringify(this.state))
+		});
+		this.history.initialize(JSON.stringify(this.state));
 
-        this.unserializer = (serialized) => {
-            this.state = JSON.parse(serialized);
-            
-            const { imageDimensions, imageSrc } = this.state;
+		this.unserializer = (serialized) => {
+			this.state = JSON.parse(serialized);
+			
+			const { imageDimensions, imageSrc } = this.state;
 
-            if (imageSrc != null) {
-                window.negative.frameController.setImageAndSize(imageSrc, imageDimensions[0], imageDimensions[1]);
-            } else {
-                window.negative.frameController.removeImage();
-            }
-        };
-    }
+			if (imageSrc != null) {
+				window.negative.frameController.setImageAndSize(imageSrc, imageDimensions[0], imageDimensions[1]);
+			} else {
+				window.negative.frameController.removeImage();
+			}
+		};
+	}
 
-    save(state) {
-        this.state = state;
-        this.history.save();
-    }
+	save(state) {
+		this.state = state;
+		this.history.save();
+	}
 
-    undo() { this.history.undo(this.unserializer); }
-    redo() { this.history.redo(this.unserializer); }
-    canUndo() { return this.history.canUndo(); }
-    canRedo() { return this.history.canRedo(); }
+	undo() { this.history.undo(this.unserializer); }
+	redo() { this.history.redo(this.unserializer); }
+	canUndo() { return this.history.canUndo(); }
+	canRedo() { return this.history.canRedo(); }
 
-    serialize() {
-        return {
-            canUndo: this.canUndo(),
-            canRedo: this.canRedo()
-        };
-    }
+	serialize() {
+		return {
+			canUndo: this.canUndo(),
+			canRedo: this.canRedo()
+		};
+	}
 }

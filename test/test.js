6521909e8193dc954bd0b4d2ba93e39e0745ce85
Adam Drago <atdrago@gmail.com>
2016-05-09 11:20:37 -0700

    Use arrow functions. Add Zoom tests.

--- test/test.js
+++ test/test.js
@@ -2,14 +2,13 @@
 
 const { Application } = require('spectron');
 const { assert } = require('chai');
-const util = require('../lib/util');
 
 const APP_PATH = './dist/Negative-darwin-x64/Negative.app/Contents/MacOS/Negative';
 
 const IMAGE_ID = '#negativeImage';
 const REGEX_BASE_64_PNG = /^data:image\/png;base64,/;
 
-describe('Negative', function () {
+describe('Negative', () => {
 	const app = new Application({
 		path: APP_PATH,
 		env: {
@@ -18,218 +17,213 @@ describe('Negative', function () {
 		}
 	});
 	
-	before(function () {
+	before(() => {
 		return app.start();
 	});
 
-	after(function () {
+	after(() => {
 		if (app && app.isRunning()) {
 			return app.stop();
 		}
 	});
 	
-	describe('Launch', function () {
-		it('Shows a window', function () {
-			return app.client.getWindowCount().then(function (count) {
-				assert.isAtLeast(count, 1);
-			});
+	describe('Launch', () => {
+		it('Shows a window', () => {
+			return app.client.getWindowCount()
+				.then((count) => assert.isAtLeast(count, 1));
 		});
 	});
 
-	describe('Menues', function () {
-		describe('Negative', function () {
-			after(function () {
-				return app.start();
-			});
+	describe('Menues', () => {
+		describe('Negative', () => {
+			after(() => app.start());
 			
 			it('About Negative');
 			it('Preferences...');
-			it('Quit Negative', function () {
+			it('Quit Negative', () => {
 				return app.electron.ipcRenderer.send('test-quit-negative')
-					.then(function () {
-						return app.client.getWindowCount().then(function (count) {
-							assert.equal(count, 0);
-						});
-					});
+					.then(() => app.client.getWindowCount())
+					.then((count) => assert.equal(count, 0));
 			});
 		});
 		
-		describe('File', function () {
-			it('New Tab', function () {
+		describe('File', () => {
+			it('New Tab', () => {
 				return app.electron.ipcRenderer.send('test-new-tab')
-					.then(function () {
-						return app.client.selectorExecute('#tabs', function (element) {
+					.then(() => {
+						return app.client.selectorExecute('#tabs', (element) => {
 							const el = element[0];
 							
 							return el.children && el.children.length;
 						});
 					})
-					.then(function (tabCount) {
-						assert.equal(tabCount, 2);
-					});
+					.then((tabCount) => assert.equal(tabCount, 2));
 			});
 			
-			it('Close Tab', function () {
+			it('Close Tab', () => {
 				return app.electron.ipcRenderer.send('test-close-tab')
-					.then(function () {
-						return app.client.selectorExecute('#tabs', function (element) {
+					.then(() => {
+						return app.client.selectorExecute('#tabs', (element) => {
 							const el = element[0];
 							
 							return el.children && el.children.length;
 						});
 					})
-					.then(function (tabCount) {
-						assert.equal(tabCount, 1);
-					});
+					.then((tabCount) => assert.equal(tabCount, 1));
 			});
 			
-			it.skip('New Window', function () {
+			it.skip('New Window', () => {
 				return app.electron.ipcRenderer.send('test-new-window')
-					.then(function () {
-						return app.client.getWindowCount();
-					})
-					.then(function (count) {
-						assert.equal(count, 2);
-					})
-					.then(function () {
-						return app.browserWindow.close();
-					});
+					.then(() => app.client.getWindowCount())
+					.then((count) => assert.equal(count, 2))
+					.then(() => app.browserWindow.close());
 			});
 			
-			it.skip('Close Window', function () {
+			it.skip('Close Window', () => {
 				return app.electron.ipcRenderer.send('test-close-window')
-					.then(function () {
-						return app.client.getWindowCount();
-					})
-					.then(function (count) {
-						assert.equal(count, 1);
-					});
+					.then(() => app.client.getWindowCount())
+					.then((count) => assert.equal(count, 1));
 			});
 			
 			it('Close');
 		});
 		
-		describe('Edit', function () {
-			it('Undo', function () {
+		describe('Edit', () => {
+			it('Undo', () => {
 				// Get some history first
 				return app.electron.ipcRenderer.send('test-clear')
-					.then(function () {
-						return app.electron.ipcRenderer.send('test-capture');
-					})
-					.then(function () {
+					.then(() => app.electron.ipcRenderer.send('test-capture'))
+					.then(() => {
 						return app.client.selectorExecute(IMAGE_ID, (element) => element[0].getAttribute('src'));
 					})
-					.then(function (src) {
-						assert.notEqual(src, '');
-					})
-					.then(function () {
-						return app.electron.ipcRenderer.send('test-undo');
-					})
-					.then(function () {
+					.then((src) => assert.notEqual(src, ''))
+					.then(() => app.electron.ipcRenderer.send('test-undo'))
+					.then(() => {
 						return app.client.selectorExecute(IMAGE_ID, (element) => element[0].getAttribute('src'));
 					})
-					.then(function (src) {
-						assert.equal(src, '');
-					});
+					.then((src) => assert.equal(src, ''));
 					
 			});
 			
-			it('Redo', function () {
+			it('Redo', () => {
 				// Use history from Undo test
 				return app.electron.ipcRenderer.send('test-redo')
-					.then(function () {
+					.then(() => {
 						return app.client.selectorExecute(IMAGE_ID, (element) => element[0].getAttribute('src'));
 					})
-					.then(function (src) {
-						assert.isTrue(REGEX_BASE_64_PNG.test(src));
-					});
+					.then((src) => assert.isTrue(REGEX_BASE_64_PNG.test(src)));
 			});
 			
-			it('Copy', function () {
+			it('Copy', () => {
 				return app.electron.ipcRenderer.send('test-capture')
-					.then(function () {
-						return app.electron.ipcRenderer.send('test-copy');
-					})
-					.then(function () {
-						return app.electron.clipboard.readImage();
-					})
-					.then(function (image) {
+					.then(() => app.electron.ipcRenderer.send('test-copy'))
+					.then(() => app.electron.clipboard.readImage())
+					.then((image) => {
 						assert.isDefined(image);
 						assert.isDefined(image.getSize);
-					})
+					});
 			});
 			
-			it('Paste', function () {
+			it('Paste', () => {
 				return app.electron.ipcRenderer.send('test-clear')
-					.then(function () {
-						return app.electron.ipcRenderer.send('test-paste');
-					})
-					.then(function () {
+					.then(() => app.electron.ipcRenderer.send('test-paste'))
+					.then(() => {
 						return app.client.selectorExecute(IMAGE_ID, (element) => element[0].getAttribute('src'));
 					})
-					.then(function (src) {
-						assert.isTrue(REGEX_BASE_64_PNG.test(src));
-					});
+					.then((src) => assert.isTrue(REGEX_BASE_64_PNG.test(src)));
 			});
 		});
 		
-		describe('View', function () {
-			it('Capture', function () {
+		describe('View', () => {
+			it('Capture', () => {
 				// Clear first, then test that capturing works
 				return app.electron.ipcRenderer.send('test-clear')
-					.then(function () {
-						return app.electron.ipcRenderer.send('test-capture');
-					})
-					.then(function () {
+					.then(() => app.electron.ipcRenderer.send('test-capture'))
+					.then(() => {
 						return app.client.selectorExecute(IMAGE_ID, (element) => element[0].getAttribute('src'));
 					})
-					.then(function (src) {
-						assert.isTrue(REGEX_BASE_64_PNG.test(src));
-					})
+					.then((src) => assert.isTrue(REGEX_BASE_64_PNG.test(src)));
 			});
 			
-			it('Clear', function () {
+			it('Clear', () => {
 				// Capture first, then test that clearing works
 				return app.electron.ipcRenderer.send('test-capture')
-					.then(function () {
-						return app.electron.ipcRenderer.send('test-clear');
+					.then(() => app.electron.ipcRenderer.send('test-clear'))
+					.then(() => {
+						return app.client.selectorExecute(IMAGE_ID, (element) => element[0].getAttribute('src'))
 					})
-					.then(function () {
-						return app.client.selectorExecute(IMAGE_ID, (element) => element[0].getAttribute('src'));
-					})
-					.then(function (src) {
-						assert.equal(src, '');
-					});
+					.then((src) => assert.equal(src, ''));
 			});
 			
-			it('Actual Size', function () {
-				let bounds;
-				
+			it('Actual Size', () => {
 				return app.electron.ipcRenderer.send('test-capture')
-					.then(function () {
-						return app.electron.ipcRenderer.send('test-zoom-in');
-					})
-					.then(function () {
-						return app.electron.ipcRenderer.send('test-actual-size');
-					})
-					.then(function () {
+					.then(() => app.electron.ipcRenderer.send('test-zoom-in'))
+					.then(() => app.electron.ipcRenderer.send('test-actual-size'))
+					.then(() => {
 						return app.client.selectorExecute(IMAGE_ID, (element) => {
 							const zoomLevel = element[0].getAttribute('data-zoom-level');
 							
 							return zoomLevel;
 						});
 					})
-					.then(function (zoomLevel) {
-						assert.equal(zoomLevel, 1);
-					});
+					.then((zoomLevel) => assert.equal(zoomLevel, 1));
 			});
-			it('Zoom In');
-			it('Zoom Out');
+			
+			it('Zoom In', () => {
+				return app.electron.ipcRenderer.send('test-capture')
+					.then(() => app.electron.ipcRenderer.send('test-actual-size'))
+					.then(() => app.electron.ipcRenderer.send('test-zoom-in'))
+					.then(() => {
+						return app.client.selectorExecute(IMAGE_ID, (element) => {
+							const zoomLevel = element[0].getAttribute('data-zoom-level');
+							
+							return zoomLevel;
+						});
+					})
+					.then((zoomLevel) => assert.equal(zoomLevel, 1.25))
+					.then(() => app.electron.ipcRenderer.send('test-zoom-in'))
+					.then(() => app.electron.ipcRenderer.send('test-zoom-in'))
+					.then(() => app.electron.ipcRenderer.send('test-zoom-in'))
+					.then(() => app.electron.ipcRenderer.send('test-zoom-in'))
+					.then(() => {
+						return app.client.selectorExecute(IMAGE_ID, (element) => {
+							const zoomLevel = element[0].getAttribute('data-zoom-level');
+							
+							return zoomLevel;
+						});
+					})
+					.then((zoomLevel) => assert.equal(zoomLevel, 2));
+			});
+			
+			it('Zoom Out', () => {
+				return app.electron.ipcRenderer.send('test-capture')
+					.then(() => app.electron.ipcRenderer.send('test-actual-size'))
+					.then(() => app.electron.ipcRenderer.send('test-zoom-out'))
+					.then(() => {
+						return app.client.selectorExecute(IMAGE_ID, (element) => {
+							const zoomLevel = element[0].getAttribute('data-zoom-level');
+							
+							return zoomLevel;
+						});
+					})
+					.then((zoomLevel) => assert.equal(zoomLevel, 0.75))
+					.then(() => app.electron.ipcRenderer.send('test-zoom-out'))
+					.then(() => app.electron.ipcRenderer.send('test-zoom-out'))
+					.then(() => {
+						return app.client.selectorExecute(IMAGE_ID, (element) => {
+							const zoomLevel = element[0].getAttribute('data-zoom-level');
+							
+							return zoomLevel;
+						});
+					})
+					.then((zoomLevel) => assert.equal(zoomLevel, 0.5));
+			});
+			
 			it('Reload');
 			it('Toggle DevTools');
 		});
 		
-		describe('Window', function () {
+		describe('Window', () => {
 			it('Minimize');
 			it('Fit Window to Image');
 			it('Next Tab');
@@ -237,20 +231,18 @@ describe('Negative', function () {
 			it('Next Tab and Resize');
 			it('Previous Tab and Resize');
 			
-			describe('Move', function () {
+			describe('Move', () => {
 				function testBounds(direction, amount) {
-					return new Promise(function (resolve, reject) {
+					return new Promise((resolve, reject) => {
 						let oldBounds;
 						
 						return app.browserWindow.getBounds()
-							.then(function (bounds) {
+							.then((bounds) => {
 								oldBounds = bounds;
 								return app.electron.ipcRenderer.send(`test-move-${direction}-${amount}`);
 							})
-							.then(function () {
-								return app.browserWindow.getBounds();
-							})
-							.then(function (newBounds) {
+							.then(() => app.browserWindow.getBounds())
+							.then((newBounds) => {
 								resolve({
 									oldBounds: oldBounds,
 									newBounds: newBounds
@@ -259,52 +251,44 @@ describe('Negative', function () {
 					});
 				}
 				
-				it('Left by 1px', function () {
-					return testBounds('left', 1).then(function (result) {
-						assert.strictEqual(result.newBounds.x, result.oldBounds.x - 1);
-					});
+				it('Left by 1px', () => {
+					return testBounds('left', 1)
+						.then((result) => assert.strictEqual(result.newBounds.x, result.oldBounds.x - 1));
 				});
 				
-				it('Right by 1px', function () {
-					return testBounds('right', 1).then(function (result) {
-						assert.strictEqual(result.newBounds.x, result.oldBounds.x + 1);
-					});
+				it('Right by 1px', () => {
+					return testBounds('right', 1)
+						.then((result) => assert.strictEqual(result.newBounds.x, result.oldBounds.x + 1));
 				});
 				
-				it('Up by 1px', function () {
-					return testBounds('up', 1).then(function (result) {
-						assert.strictEqual(result.newBounds.y, result.oldBounds.y - 1);
-					});
+				it('Up by 1px', () => {
+					return testBounds('up', 1)
+						.then((result) => assert.strictEqual(result.newBounds.y, result.oldBounds.y - 1));
 				});
 				
-				it('Down by 1px', function () {
-					return testBounds('down', 1).then(function (result) {
-						assert.strictEqual(result.newBounds.y, result.oldBounds.y + 1);
-					});
+				it('Down by 1px', () => {
+					return testBounds('down', 1)
+						.then((result) => assert.strictEqual(result.newBounds.y, result.oldBounds.y + 1));
 				});
 				
-				it('Left by 10px', function () {
-					return testBounds('left', 10).then(function (result) {
-						assert.strictEqual(result.newBounds.x, result.oldBounds.x - 10);
-					});
+				it('Left by 10px', () => {
+					return testBounds('left', 10)
+						.then((result) => assert.strictEqual(result.newBounds.x, result.oldBounds.x - 10));
 				});
 				
-				it('Right by 10px', function () {
-					return testBounds('right', 10).then(function (result) {
-						assert.strictEqual(result.newBounds.x, result.oldBounds.x + 10);
-					});
+				it('Right by 10px', () => {
+					return testBounds('right', 10)
+						.then((result) => assert.strictEqual(result.newBounds.x, result.oldBounds.x + 10));
 				});
 				
-				it('Up by 10px', function () {
-					return testBounds('up', 10).then(function (result) {
-						assert.strictEqual(result.newBounds.y, result.oldBounds.y - 10);
-					});
+				it('Up by 10px', () => {
+					return testBounds('up', 10)
+						.then((result) => assert.strictEqual(result.newBounds.y, result.oldBounds.y - 10));
 				});
 				
-				it('Down by 10px', function () {
-					return testBounds('down', 10).then(function (result) {
-						assert.strictEqual(result.newBounds.y, result.oldBounds.y + 10);
-					});
+				it('Down by 10px', () => {
+					return testBounds('down', 10)
+						.then((result) => assert.strictEqual(result.newBounds.y, result.oldBounds.y + 10));
 				});
 			});
 		});

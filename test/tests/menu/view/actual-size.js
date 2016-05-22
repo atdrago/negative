6d83de3928464ff11f1f685b35df5ad65b7899bf
Adam Drago <atdrago@gmail.com>
2016-05-22 15:54:08 -0700

    Base "Actual Size" test inital state on fixture.

--- test/tests/menu/view/actual-size.js
+++ test/tests/menu/view/actual-size.js
@@ -6,7 +6,7 @@ const { assert } = require('chai');
 const APP_PATH = './dist/Negative-darwin-x64/Negative.app/Contents/MacOS/Negative';
 const IMAGE_ID = '#negativeImage';
 
-describe('View > Actual Size', function () {
+describe.only('View > Actual Size', function () {
 	const app = new Application({
 		path: APP_PATH,
 		env: {
@@ -14,7 +14,7 @@ describe('View > Actual Size', function () {
 			ELECTRON_ENABLE_STACK_DUMPING: true,
 			NEGATIVE_IGNORE_WINDOW_SETTINGS: false,
 			NEGATIVE_SKIP_RESET_DIALOG: true,
-			NEGATIVE_SETTINGS_PATH: '../test/fixtures/two-windows-with-data.json',
+			NEGATIVE_SETTINGS_PATH: '../test/fixtures/two-windows-with-data-after-zoom.json',
 			NODE_ENV: 'development'
 		}
 	});
@@ -32,9 +32,7 @@ describe('View > Actual Size', function () {
 	});
 	
 	it('Should zoom to 0', () => {
-		// @TODO - Load settings file for actual size. Blocked by #96.
 		return app.client.waitUntilWindowLoaded()
-			.then(() => app.electron.ipcRenderer.send('test-zoom-in'))
 			.then(() => app.electron.ipcRenderer.send('test-actual-size'))
 			.then(() => {
 				return app.client.selectorExecute(IMAGE_ID, (element) => {

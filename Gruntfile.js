6714f48058a03437ad5c2fa1331a388f7b0551ad
Adam Drago <atdrago@gmail.com>
2016-05-26 08:10:39 -0700

    Bump Electron to 1.1.3

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,7 @@ grunt.initConfig({
 				name: 'Negative',
 				icon: 'negative.icns',
 				arch: 'x64',
-				version: '1.1.1',
+				version: '1.1.3',
 				platform: 'darwin',
 				out: 'dist',
 				dir: 'release',
